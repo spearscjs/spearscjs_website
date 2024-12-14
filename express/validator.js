96b4d49f86e63618f69469eb0d2cc5fae6280a49
spearscjs <spearscjs@gmail.com>
2024-12-13 22:22:44 -0500

    Update validator.js

--- express/validator.js
+++ express/validator.js
@@ -24,7 +24,6 @@ function validateInput(request) {
     return errors;
 }
 
-// I stole this function and its regex from stackoverflow
 const validateEmail = (email) => {
     return email.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
